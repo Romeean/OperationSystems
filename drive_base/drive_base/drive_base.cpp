b98b4085e8054fa11275b78c605d586ae0696c70
Roman <topmanov@gmail.com>
2026-01-27 19:06:33 +0200

    Синхронизация

---
+++ drive_base/drive_base/drive_base.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+using namespace std;
+
+
+int main()
+{
+	int choice = -1;
+
+	while (true) {
+		cout << "\n";
+		cout << "1. Information about drives" << endl;
+		cout << "2. Information about Operation System" << endl;
+		cout << "3. All file's attributes in specific directory" << endl;
+		cout << "4. All files with .txt extension" << endl;
+		if (!(cin >> choice)) {
+			cout << "Unknown command \n";
+			cin.clear();
+			cin.ignore((numeric_limits<streamsize>::max)(), '\n');
+		}
+
+		switch (choice) {
+		
+		case 1: {
+
+		};
+		case 2: {
+
+		}
+		case 3: {
+
+		}
+		case 4: {
+
+		}
+		}
+	}
+
+
+}
