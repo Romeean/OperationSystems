d3e6df7c5bac1e83aaecda906492f9fc6a8b946b
Roman <topmanov@gmail.com>
2026-02-21 11:38:07 +0300

    Completed a project related to the anonymous pipe.

--- pipe/pipe_client/pipe_client/pipe_client.cpp
+++ pipe/pipe_client/pipe_client/pipe_client.cpp
@@ -1,15 +1,81 @@
 ﻿#include <iostream>
 #include <windows.h>
+#include <vector>
 
 using namespace std;
+
+vector<int> merge(vector<int>& left, vector<int>& right) {
+	int leftIndex = 0;
+	int rightIndex = 0;
+
+	vector<int> result;
+
+	while (leftIndex < left.size() && rightIndex < right.size()) {
+		if (left[leftIndex] > right[rightIndex]) {
+			result.push_back(right[rightIndex]);
+			rightIndex++;
+		}
+		else {
+			result.push_back(left[leftIndex]);
+			leftIndex++;
+		}
+	}
+	while (leftIndex < left.size()) {
+		result.push_back(left[leftIndex]);
+		leftIndex++;
+	}
+	while (rightIndex < right.size()) {
+		result.push_back(right[rightIndex]);
+		rightIndex++;
+	}
+	return result;
+}
+
+vector<int> merge_sort(vector<int> &array) {
+	if (array.size() == 1) return array;
+	int middle = array.size() / 2;
+
+	int length = array.size();
+	
+	vector<int> left(array.begin(), array.begin() + middle);
+	vector<int> right(array.begin() + middle, array.end());
+
+	left  = merge_sort(left);
+	right = merge_sort(right);
+
+	return merge(left, right);
+}
 
 
 int main()
 {
-	DWORD dwRead;
-	
-	//ReadFile(hReadPipe)
+	HANDLE hInput = GetStdHandle(STD_INPUT_HANDLE);
+	DWORD bytesRead;
+	size_t length;
 
+	if (!ReadFile(hInput, &length, sizeof(size_t), &bytesRead, 0)) {
+		cout << "Read length error: " << GetLastError() << endl;
+		return 1;
+	};
+
+	vector<int> loadedArray(length);
+	if (!ReadFile(hInput, loadedArray.data(), length * sizeof(int), &bytesRead, 0)) {
+		cout << "Read array error: " << GetLastError() << endl;
+		return 1;
+	}
+
+	cout << "sorted array" << endl;
+	
+	vector<int> result = merge_sort(loadedArray);
+	cout << "[ ";
+	for (int i = 0; i < result.size(); i++) {
+		if (i == result.size() - 1) {
+			cout << result[i] << " ";
+			break;
+		}
+		cout << result[i] << ", ";
+	}
+	cout << "]";
 
 	return 0;
-}
+}	
--- pipe/pipe_server/pipe_server/pipe_server.cpp
+++ pipe/pipe_server/pipe_server/pipe_server.cpp
@@ -2,17 +2,18 @@
 #include <windows.h>
 #include <vector>
 
-
 using namespace std;
 
 
-wchar_t path[] = L"C:\\Users\\user\\source\\repos\\OperationSystems\\pipe\\pipe_client\\x64\\Debug\\pipe_client.exe";
+//wchar_t path_pc[] = L"C:\\Users\\user\\source\\repos\\OperationSystems\\pipe\\pipe_client\\x64\\Debug\\pipe_client.exe";
+wchar_t path_desctop[] = L"C:\\Users\\Владимир\\source\\repos\\OperationSystems\\pipe\\pipe_client\\x64\\Debug\\pipe_client.exe";
+
 
 int main()
 {
 	HANDLE hWritePipe, hReadPipe;
 	DWORD dwSize = 0;
-	
+
 	PROCESS_INFORMATION pi;
 	SECURITY_ATTRIBUTES sa;
 	STARTUPINFO si;
@@ -21,89 +22,50 @@ int main()
 	sa.bInheritHandle = TRUE;
 	sa.lpSecurityDescriptor = NULL;
 
+	if (!CreatePipe(&hReadPipe, &hWritePipe, &sa, dwSize)) {
+		cout << "Error with pipe: " << GetLastError() << endl;
+	}
+
 	ZeroMemory(&si, sizeof(si));
 	si.cb = sizeof(si);
 	ZeroMemory(&pi, sizeof(pi));
 
-	if (!CreatePipe(&hReadPipe, &hWritePipe, &sa, dwSize)) {
-		cout << "Error with pipe: " << GetLastError() << endl;
-	}
-	if (!CreateProcess(NULL, path, NULL, NULL, TRUE, 0, NULL, NULL, &si, &pi)) {
+	si.dwFlags = STARTF_USESTDHANDLES;
+
+	si.hStdInput = hReadPipe;
+	si.hStdOutput = GetStdHandle(STD_OUTPUT_HANDLE);
+	si.hStdError = GetStdHandle(STD_ERROR_HANDLE);
+
+
+	if (!CreateProcess(NULL, path_desctop, NULL, NULL, TRUE, 0, NULL, NULL, &si, &pi)) {
 		cout << "Error with process: " << GetLastError() << endl;
 	}
-
-	si.hStdInput	= hReadPipe;
-	si.hStdOutput = GetStdHandle(STD_OUTPUT_HANDLE);
-	si.hStdError	= GetStdHandle(STD_ERROR_HANDLE);
-
-
 		
-
-
+	cout << "Create an array and sent it to the client." << endl;
 	
-	
+	vector<int> array;
+	size_t length;
 
-
-	int choice = -1;
-	while (true) {
-		
-		cout << "What you want to send to client?" << endl;
-		cout << "1. create an array and sent it to the client and sort it." << endl;
-		cout << "2. create an array and sent it to the client and cout all of the unique elements.  " << endl;
-		
-		while (!(cin >> choice)) {
-			cout << "Invalid input. Try again";
+	cout << "Enter desired length of the array: ";
+	while (!(cin >> length)) {
+		cout << "Invalid input. Try again" << endl;
+		cin.clear();
+		cin.ignore((numeric_limits<streamsize>::max)(), '\n');
+	}
+	for (size_t i = 0; i < length; i++) {
+		int number;
+		cout << "Enter " << i + 1 << "value is: ";
+		while (!(cin >> number)) {
+			cout << "Invalid input. Try again" << endl;
 			cin.clear();
 			cin.ignore((numeric_limits<streamsize>::max)(), '\n');
 		}
-
-
-		switch (choice) {
-		
-		case 1: {
-			vector<int> array;
-			size_t length;
-
-			cout << "Enter desired length of the array: ";
-			while (!(cin >> length)) {
-				cout << "Invalid input. Try again" << endl;
-				cin.clear();
-				cin.ignore((numeric_limits<streamsize>::max)(), '\n');
-			}
-
-			for (size_t i = 0; i < length; i++) {
-				int number;
-				cout << "Enter " << i + 1 << "value is: ";
-				while (!(cin >> number)) {
-					cout << "Invalid input. Try again" << endl;
-					cin.clear();
-					cin.ignore((numeric_limits<streamsize>::max)(), '\n');
-				}
-				array.push_back(number);
-			}
-			DWORD dwWritten;
-			// передача с помощьб pipe значения длинны массива который ввел пользователь
-			WriteFile(hWritePipe, &length, sizeof(int), &dwWritten, NULL);
-			
-			WriteFile(hWritePipe, array.data(), length & sizeof(int), &dwWritten, NULL);
-
-
-
-		}
-
-		case 2: {
-
-		}
-		default: {
-
-		}
-		}
-		cout << endl;
+		array.push_back(number);
 	}
-
-
-
-
+	
+	DWORD dwWritten;
+	WriteFile(hWritePipe, &length, sizeof(size_t), &dwWritten, NULL); 
+	WriteFile(hWritePipe, array.data(), length * sizeof(int), &dwWritten, NULL); 
 
 	CloseHandle(hWritePipe);
 	CloseHandle(hReadPipe);
