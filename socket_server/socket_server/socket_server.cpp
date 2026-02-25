08c3f2f511c57ee13c6ee3d93e824dafd227fefd
Roman <topmanov@gmail.com>
2026-02-25 08:34:58 +0300

    socket implementation completed.

--- socket_server/socket_server/socket_server.cpp
+++ socket_server/socket_server/socket_server.cpp
@@ -12,11 +12,10 @@ int main() {
   ZeroMemory(&hints, sizeof(hints));
   hints.ai_family   = AF_INET;     // IPv4
   hints.ai_socktype = SOCK_STREAM; // TCP 
-  hints.ai_protocol = IPPROTO_TCP; //
-  hints.ai_flags    = AI_PASSIVE;  //
+  hints.ai_protocol = IPPROTO_TCP; 
+  hints.ai_flags    = AI_PASSIVE; 
 
-  // NULL + AI_PASSIVE позволяет принимать подключения на любой адрес (в т.ч. 127.0.0.1)
-  getaddrinfo(NULL, "12345", &hints, &result);
+  getaddrinfo("127.0.0.1", "12345", &hints, &result);
 
   SOCKET customSocket = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
   bind(customSocket, result->ai_addr, (int)result->ai_addrlen);
@@ -26,32 +25,26 @@ int main() {
   cout << "[SERVER] is on port 12345. Waiting..." << endl;
 
 
-
   while (true) {
     sockaddr_in clientAddr;
     int clientAddrSize = sizeof(clientAddr);
 
-    // 1. Принимаем клиента
     SOCKET clientSocket = accept(customSocket, (sockaddr*)&clientAddr, &clientAddrSize);
 
     if (clientSocket != INVALID_SOCKET) {
-      cout << "> Client connected!" << endl;
+      cout << "Client connected" << endl;
 
       char buffer[1024];
-      // 2. Читаем данные от клиента (один раз)
       int bytesReceived = recv(clientSocket, buffer, sizeof(buffer), 0);
 
       if (bytesReceived > 0) {
-        // Добавляем нулевой символ, чтобы вывести как строку
         buffer[bytesReceived] = '\0';
         cout << "Received: " << buffer << endl;
 
-        // 3. Отправляем ответ (Эхо)
         string msg = "Hello from Server!";
         send(clientSocket, msg.c_str(), (int)msg.length(), 0);
       }
 
-      // 4. Закрываем связь с этим клиентом
       closesocket(clientSocket);
       cout << "> Client served and closed." << endl;
     }
